2b6f70a3a14a442a988612386ec1806152926965
ed1rac <edkallenn@gmail.com>
2018-02-03 01:14:27 -0500

    Script de instalacao do Alpine

---
+++ instala-alpine.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+principal() {
+	echo "Script de inicialização Geral do Ed - Debian"
+	sleep 2
+	#cd /home/edkallenn/
+	#read -p "Qual a pasta que vc deseja copiar o coneteudo das Ferramentas? " pasta1
+	#echo 'A pasta escolhida foi: ' $pasta1
+	#$pasta1 = '~'	
+	#apt-get install fortunes -y
+	#apt-get install fortunes-br -y
+	cd $HOME
+	pasta='./FerramentasProgramacao/'
+	
+	if [ -d $pasta ]; then
+    		echo "O diretório $pasta já existe"
+		#copiar
+	else
+    		git clone https://github.com/ed1rac/FerramentasProgramacao.git
+    		copiar
+	fi
+	executa
+}
+
+copiar() {
+    #raiz='/home/edkallenn/FerramentasProgramacao/'
+    #raiz=$pasta1'/FerramentasProgramacao/'
+    #echo $raiz
+    #cd $raiz
+    cd $HOME/FerramentasProgramacao/
+    pwd
+    cp bash-profile/bash-ubuntu-on-windows/.bashrc $HOME -r -f -v
+    cp nano/nanorc /etc -f -v
+    cp vim-temas/.vim ~ -r -f && cp vim-temas/.vimrc $HOME -r -f -v
+    cd $HOME
+}
+
+executa() {
+    #cd ~
+    source $HOME/.bashrc
+    #/usr/games/fortune | /usr/games/cowsay -f tux
+}
+
+principal
