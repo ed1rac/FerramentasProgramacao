6831a6abfd1c1c2942d9ac51bc53aaa3177df74e
Ed1rac (Edkallenn Lima) <ed1rac@users.noreply.github.com>
2022-02-23 16:18:23 -0500

    Create config_proxy.sh

---
+++ config_proxy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+principal(){
+echo "Script de configuração do proxy do Ed"
+sleep 2
+arquivo="/etc/apt/apt.conf.d/80proxy"
+if [ -f $arquivo ]; then
+	echo "O arquivo $pasta já existe"  		
+else
+	proxy
+fi
+executa
+}
+
+proxy(){
+	touch /etc/apt/apt.conf.d/80proxy
+	echo "Acquire::http::proxy  \"http://<usuario>:<senha>@proxy.dpf.gov.br:3128/\";" >> /etc/apt/apt.conf.d/80proxy
+	echo "Acquire::ftp::proxy   \"http://<usuario>:<senha>@proxy.dpf.gov.br:3128/\";" >> /etc/apt/apt.conf.d/80proxy
+	echo "Acquire::https::proxy \"http://<usuario>:<senha>@proxy.dpf.gov.br:3128/\";" >> /etc/apt/apt.conf.d/80proxy
+	echo "export https_proxy=http://username:password@proxy.incolume.com.br:3128" >> ~/.bashrc
+	echo "export http_proxy=http://username:password@proxy.incolume.com.br:3128" >> ~/.bashrc
+	echo "export ftp_proxy=http://username:password@proxy.incolume.com.br:3128" >> ~/.bashrc	
+}
+
+executa() {
+    #cd ~
+    echo "Script executado com sucesso"
+    source $HOME/.bashrc
+    #EXPORT DISPLAY=:0  #Para o WSL 'visual' usando Xming
+}
+
+principal
