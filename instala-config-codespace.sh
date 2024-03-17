ea5ca9b0ffa675499e2d7aba3d20095d60ce10f1
Ed1rac (Edkallenn Lima) <ed1rac@users.noreply.github.com>
2024-03-17 11:07:53 -0300

    mudando o .bashrc

--- instala-config-codespace.sh
+++ instala-config-codespace.sh
@@ -55,11 +55,14 @@ executa() {
 }
 
 copia_oh_my_posh(){
-	curl -s https://ohmyposh.dev/install.sh | bash -s
-	/usr/local/bin/oh-my-posh font install 3270
-	/usr/local/bin/oh-my-posh font install CascadiaCode
-	/usr/local/bin/oh-my-posh font install Arimo
-	/usr/local/bin/oh-my-posh font install DroidSansMono
+	#curl -s https://ohmyposh.dev/install.sh | bash -s
+	curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~
+	~/oh-my-posh font install 3270
+	~/oh-my-posh font install CascadiaCode
+	~/oh-my-posh font install Arimo
+	~/oh-my-posh font install DroidSansMono
+	#oh-my-posh init bash --config /amro.omp.json
+	eval "$(~/oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/amro.omp.json)"
 }
 
 principal
