114dd75eb357d9c0b03eadee7d0fb3ab7ca13c72
Ed1rac (Edkallenn Lima) <ed1rac@users.noreply.github.com>
2024-03-17 20:47:48 -0300

    Update instala-config-codespace.sh

--- instala-config-codespace.sh
+++ instala-config-codespace.sh
@@ -25,7 +25,9 @@ principal() {
     	copiar
 	#fi
 	executa
-	#copia_oh_my_posh
+ 	echo "Alterando o prompt"
+	sleep 2
+	copia_oh_my_posh
 }
 
 copiar() {
@@ -56,13 +58,12 @@ executa() {
 
 copia_oh_my_posh(){
 	#curl -s https://ohmyposh.dev/install.sh | bash -s
-	curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~
+	curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /home/vscode
 	~/oh-my-posh font install 3270
 	~/oh-my-posh font install CascadiaCode
 	~/oh-my-posh font install Arimo
 	~/oh-my-posh font install DroidSansMono
 	#oh-my-posh init bash --config /amro.omp.json
 	eval "$(~/oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/amro.omp.json)"
-}
 
 principal
