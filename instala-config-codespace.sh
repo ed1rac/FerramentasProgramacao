7f5f6b93400454e94d647a7709177187e76c745a
Ed1rac (Edkallenn Lima) <ed1rac@users.noreply.github.com>
2024-03-18 00:27:04 +0000

    Trocando LRF por LF

--- instala-config-codespace.sh
+++ instala-config-codespace.sh
@@ -12,50 +12,47 @@ principal() {
 	#cd /home/edkallenn/
 	#read -p "Qual a pasta que vc deseja copiar o coneteudo das Ferramentas? (/home/edkallenn) " pasta1
 	#echo 'A pasta escolhida foi: ' $pasta1
-	cd $HOME	
+	cd $HOME
 	sleep 2
 	#pasta=$HOME'/FerramentasProgramacao/'
-
+	
 	#if [ -d $pasta ]; then
-  #  	echo "O diretório $pasta já existe"
+	#  	echo "O diretório $pasta já existe"
 	#	copiar
 	#else
 	#cd $pasta
-    	git clone https://github.com/ed1rac/FerramentasProgramacao.git
-    	copiar
+	git clone https://github.com/ed1rac/FerramentasProgramacao.git
+	copiar
 	#fi
 	executa
- 	echo "Alterando o prompt"	
+	echo "Alterando o prompt"
 	sleep 2
 	copia_oh_my_posh
 }
-
 copiar() {
-    #raiz='/home/edkallenn/FerramentasProgramacao/'
-    #raiz=$pasta1'/FerramentasProgramacao/'	
-	  raiz=$HOME'/FerramentasProgramacao/'	
-    #echo $raiz	
-    #cd $raiz
-    cd $HOME'/FerramentasProgramacao/'
-    pwd
-    cp bash-profile/bash-ubuntu-on-windows/.bashrc ~ -r -f -v
-    cp nano/nanorc /etc -f -v
-    cp vim-temas/.vim ~ -r -f && cp vim-temas/.vimrc ~ -r -f -v
-    cd ~
+	#raiz='/home/edkallenn/FerramentasProgramacao/'
+	#raiz=$pasta1'/FerramentasProgramacao/'
+	raiz=$HOME'/FerramentasProgramacao/'
+	#echo $raiz
+	#cd $raiz
+	cd $HOME'/FerramentasProgramacao/'
+	pwd
+	cp bash-profile/bash-ubuntu-on-windows/.bashrc ~ -r -f -v
+	cp nano/nanorc /etc -f -v
+	cp vim-temas/.vim ~ -r -f && cp vim-temas/.vimrc ~ -r -f -v
+	cd ~
 }
-
 executa() {
-  #cd ~
+	#cd ~
 	echo "Instalação do Fortunes-b e o Cowsay"
 	apt-get install fortunes-br
-	apt install cowsay	
+	apt install cowsay
 	echo "export DISPLAY=:0" >> $HOME/.bashrc  	#Para o WSL 'visual' usando Xming
 	echo "/usr/games/fortune | /usr/games/cowsay -f tux" >> ~/.bashrc
-  	source $HOME/.bashrc
-  	cd $HOME
-  	#xrandr -s aResoluçãoEscolhida >> .profile  # linha para configurar a resolução no Bodhi Linux
+	source $HOME/.bashrc
+	cd $HOME
+	#xrandr -s aResoluçãoEscolhida >> .profile  # linha para configurar a resolução no Bodhi Linux
 }
-
 copia_oh_my_posh(){
 	#curl -s https://ohmyposh.dev/install.sh | bash -s
 	curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /home/vscode
@@ -65,5 +62,6 @@ copia_oh_my_posh(){
 	~/oh-my-posh font install DroidSansMono
 	#oh-my-posh init bash --config /amro.omp.json
 	eval "$(~/oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/amro.omp.json)"
+}
 
 principal
